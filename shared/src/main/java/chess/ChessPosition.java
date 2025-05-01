ee7e580830342747add1043be2440af99575055c
richachr <techzgamingdevs@gmail.com>
2025-04-30 22:53:24 -0600

    Added member variables for constructor. Implemented getRow, getArrayRow, getColumn, getArrayColumn.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,7 +8,12 @@ package chess;
  */
 public class ChessPosition {
 
+    private final int row;
+    private final int col;
+
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -16,7 +21,11 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
+    }
+
+    public int getArrayRow() {
+        return 8 - row;
     }
 
     /**
@@ -24,6 +33,10 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
+    }
+
+    public int getArrayColumn() {
+        return col - 1;
     }
 }
