388d15e376c227ad54cbd2164dcb9923bbb59316
richachr <techzgamingdevs@gmail.com>
2025-05-02 16:23:34 -0600

    Implemented class.

---
+++ shared/src/main/java/chess/KingMovesGenerator.java
@@ -0,0 +1,20 @@
+package chess;
+
+public class KingMovesGenerator extends MovesGenerator {
+    public KingMovesGenerator(ChessBoard board, ChessPosition position) {
+        super(board, position);
+        possibleMoves.add(new ChessPosition(position.getRow()-1,position.getColumn()-1));
+        possibleMoves.add(new ChessPosition(position.getRow()-1, position.getColumn()));
+        possibleMoves.add(new ChessPosition(position.getRow()-1,position.getColumn()+1));
+        possibleMoves.add(new ChessPosition(position.getRow(),position.getColumn()-1));
+        possibleMoves.add(new ChessPosition(position.getRow(),position.getColumn()+1));
+        possibleMoves.add(new ChessPosition(position.getRow()+1,position.getColumn()-1));
+        possibleMoves.add(new ChessPosition(position.getRow()+1, position.getColumn()));
+        possibleMoves.add(new ChessPosition(position.getRow()+1,position.getColumn()+1));
+        for(ChessPosition newPosition : possibleMoves) {
+            if(newPosition.isInBounds() && (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != board.getPiece(position).getTeamColor())) {
+                validMoves.add(new ChessMove(position, newPosition, null));
+            }
+        }
+    }
+}
