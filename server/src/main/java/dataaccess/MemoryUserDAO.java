cf23aeca2bc3ac197b58dda726b98ad91e529a0a
richachr <techzgamingdevs@gmail.com>
2025-05-17 20:46:38 -0600

    Implemented UserDAO.

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,27 @@
 package dataaccess;
 
-public class MemoryUserDAO {
+import model.AuthData;
+import model.UserData;
+
+import java.util.ArrayList;
+
+public class MemoryUserDAO implements UserDAO {
+    private static ArrayList<UserData> mainArray = new ArrayList<>();
+
+    public void createUser(UserData data) {
+        mainArray.add(data);
+    }
+
+    public UserData getUser(String username) {
+        for(UserData data : mainArray) {
+            if(username.equals(data.username())) {
+                return data;
+            }
+        }
+        return null;
+    }
+
+    public void clear() {
+        mainArray.clear();
+    }
 }
