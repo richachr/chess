4ef89a13fe995875d63953559b06b6818b637d53
richachr <techzgamingdevs@gmail.com>
2025-05-17 20:39:17 -0600

    Implemented GameDAO using in-memory storage.

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,35 @@
 package dataaccess;
 
-public class MemoryGameDAO {
+import model.GameData;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MemoryGameDAO implements GameDAO {
+    private static List<GameData> mainArray;
+
+    public void createGame(GameData data) {
+        mainArray.add(data);
+    }
+
+    public GameData getGame(int gameID) {
+        for(GameData data : mainArray) {
+            if(data.gameID() == gameID) {
+                return data;
+            }
+        }
+        return null;
+    }
+
+    public Collection<GameData> listGames() {
+        return mainArray;
+    }
+
+    public void updateGame(GameData original, GameData replacement) {
+        mainArray.set(mainArray.indexOf(original), replacement);
+    }
+
+    public void clear() {
+        mainArray.clear();
+    }
 }
