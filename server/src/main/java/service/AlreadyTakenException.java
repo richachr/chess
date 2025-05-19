aaacca5d893f16c7531c809af181b409a3ed9054
richachr <techzgamingdevs@gmail.com>
2025-05-19 16:05:45 -0600

    Added default constructor.

--- server/src/main/java/service/AlreadyTakenException.java
+++ server/src/main/java/service/AlreadyTakenException.java
@@ -1,6 +1,11 @@
 package service;
 
 public class AlreadyTakenException extends RuntimeException {
+
+    public AlreadyTakenException() {
+        super("already taken");
+    }
+
     public AlreadyTakenException(String message) {
         super(message);
     }
--- server/src/main/java/service/BadRequestException.java
+++ server/src/main/java/service/BadRequestException.java
@@ -1,6 +1,11 @@
 package service;
 
 public class BadRequestException extends RuntimeException {
+
+    public BadRequestException() {
+        super("bad request");
+    }
+
     public BadRequestException(String message) {
         super(message);
     }
