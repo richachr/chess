67209638fb238c4025eb5a754cbd3446507d99aa
richachr <techzgamingdevs@gmail.com>
2025-05-19 15:20:00 -0600

    Implemented clear method.

--- server/src/main/java/service/TestService.java
+++ server/src/main/java/service/TestService.java
@@ -1,4 +1,16 @@
 package service;
 
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+
 public class TestService {
+    public void clear() {
+        var users = new MemoryUserDAO();
+        var auths = new MemoryAuthDAO();
+        var games = new MemoryGameDAO();
+        users.clear();
+        auths.clear();
+        games.clear();
+    }
 }
