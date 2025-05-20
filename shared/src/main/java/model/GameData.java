12635b89248a374b6593364b91ab7d8f65623811
richachr <techzgamingdevs@gmail.com>
2025-05-19 18:28:27 -0600

    Added isTaken method for team selection.

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -3,4 +3,11 @@ package model;
 import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    public boolean isTaken(String color) {
+        switch(color.toUpperCase()) {
+            case "WHITE" -> {return this.whiteUsername != null;}
+            case "BLACK" -> {return this.blackUsername != null;}
+            default -> throw new RuntimeException("Unexpected team color.");
+        }
+    }
 }
