c6cc56df3bb8bf756654f1a94c47b4329ee5d08e
richachr <techzgamingdevs@gmail.com>
2025-05-19 18:57:35 -0600

    Implemented register endpoint and handler.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,12 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import request.RegisterRequest;
+import result.ErrorResult;
+import service.AlreadyTakenException;
+import service.BadRequestException;
+import service.UserService;
 import spark.*;
 
 public class Server {
@@ -10,6 +17,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/game",(req, res) -> new RegisterHandler());
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
@@ -21,5 +29,30 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+}
+
+class RegisterHandler implements Route {
+    public Object handle(Request req, Response res) {
+        var gsonBuilder = new GsonBuilder().create();
+        RegisterRequest RegisterReq = gsonBuilder.fromJson(req.body(), RegisterRequest.class);
+        try {
+            var RegisterRes = UserService.register(RegisterReq);
+            res.status(200);
+            res.body(gsonBuilder.toJson(RegisterRes));
+        } catch (BadRequestException e) {
+            res.status(400);
+            var errorResult = new ErrorResult(e.getLocalizedMessage());
+            res.body(gsonBuilder.toJson(errorResult));
+        } catch (AlreadyTakenException e) {
+            res.status(403);
+            var errorResult = new ErrorResult(e.getLocalizedMessage());
+            res.body(gsonBuilder.toJson(errorResult));
+        } catch (Exception e) {
+            res.status(500);
+            var errorResult = new ErrorResult(e.getLocalizedMessage());
+            res.body(gsonBuilder.toJson(errorResult));
+        }
+        return res;
     }
 }
