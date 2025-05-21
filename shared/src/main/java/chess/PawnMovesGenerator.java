34a12e9a60659f0a6a4941ba0db9653aa1190da4
richachr <techzgamingdevs@gmail.com>
2025-05-20 18:00:54 -0600

    Moved repeated code into method.

--- shared/src/main/java/chess/PawnMovesGenerator.java
+++ shared/src/main/java/chess/PawnMovesGenerator.java
@@ -15,36 +15,15 @@ public class PawnMovesGenerator extends MovesGenerator {
                         validMoves.add(new ChessMove(position,doubleTestPosition,null));
                     }
                 }
-                if(isPromotable(testPosition.getRow(),ownColor)) {
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.ROOK));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.KNIGHT));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.BISHOP));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.QUEEN));
-                } else {
-                    validMoves.add(new ChessMove(position,testPosition,null));
-                }
+                checkPromotionAndAdd(position,testPosition);
             }
             testPosition = new ChessPosition(row-1,col+1);
             if(testPosition.isInBounds() && board.getPiece(testPosition) != null && board.getPiece(testPosition).getTeamColor() != ownColor) {
-                if(isPromotable(testPosition.getRow(),ownColor)) {
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.ROOK));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.KNIGHT));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.BISHOP));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.QUEEN));
-                } else {
-                    validMoves.add(new ChessMove(position,testPosition,null));
-                }
+                checkPromotionAndAdd(position,testPosition);
             }
             testPosition = new ChessPosition(row-1,col-1);
             if(testPosition.isInBounds() && board.getPiece(testPosition) != null && board.getPiece(testPosition).getTeamColor() != ownColor) {
-                if(isPromotable(testPosition.getRow(),ownColor)) {
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.ROOK));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.KNIGHT));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.BISHOP));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.QUEEN));
-                } else {
-                    validMoves.add(new ChessMove(position,testPosition,null));
-                }
+                checkPromotionAndAdd(position,testPosition);
             }
         }
         // White pawns
@@ -57,41 +36,31 @@ public class PawnMovesGenerator extends MovesGenerator {
                         validMoves.add(new ChessMove(position,doubleTestPosition,null));
                     }
                 }
-                if(isPromotable(testPosition.getRow(),ownColor)) {
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.ROOK));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.KNIGHT));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.BISHOP));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.QUEEN));
-                } else {
-                    validMoves.add(new ChessMove(position,testPosition,null));
-                }
+                checkPromotionAndAdd(position,testPosition);
             }
             testPosition = new ChessPosition(row+1,col+1);
             if(testPosition.isInBounds() && board.getPiece(testPosition) != null && board.getPiece(testPosition).getTeamColor() != ownColor) {
-                if(isPromotable(testPosition.getRow(),ownColor)) {
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.ROOK));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.KNIGHT));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.BISHOP));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.QUEEN));
-                } else {
-                    validMoves.add(new ChessMove(position,testPosition,null));
-                }
+                checkPromotionAndAdd(position,testPosition);
             }
             testPosition = new ChessPosition(row+1,col-1);
             if(testPosition.isInBounds() && board.getPiece(testPosition) != null && board.getPiece(testPosition).getTeamColor() != ownColor) {
-                if(isPromotable(testPosition.getRow(),ownColor)) {
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.ROOK));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.KNIGHT));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.BISHOP));
-                    validMoves.add(new ChessMove(position,testPosition, ChessPiece.PieceType.QUEEN));
-                } else {
-                    validMoves.add(new ChessMove(position,testPosition,null));
-                }
+                checkPromotionAndAdd(position, testPosition);
             }
         }
     }
 
     private boolean isPromotable(int row, ChessGame.TeamColor color) {
         return ((color == ChessGame.TeamColor.BLACK && row == 1) || (color == chess.ChessGame.TeamColor.WHITE && row == 8));
+    }
+
+    public void checkPromotionAndAdd(ChessPosition originalPosition, ChessPosition testPosition) {
+        if(isPromotable(testPosition.getRow(),ownColor)) {
+            validMoves.add(new ChessMove(originalPosition,testPosition, ChessPiece.PieceType.ROOK));
+            validMoves.add(new ChessMove(originalPosition,testPosition, ChessPiece.PieceType.KNIGHT));
+            validMoves.add(new ChessMove(originalPosition,testPosition, ChessPiece.PieceType.BISHOP));
+            validMoves.add(new ChessMove(originalPosition,testPosition, ChessPiece.PieceType.QUEEN));
+        } else {
+            validMoves.add(new ChessMove(originalPosition,testPosition,null));
+        }
     }
 }
