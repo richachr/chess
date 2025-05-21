2949df42c38a0d327801a743592150ac860f964e
richachr <techzgamingdevs@gmail.com>
2025-05-20 18:01:28 -0600

    Added checkAndAddPossibleMoves to remove duplicate code from MovesGenerator subclasses..

--- shared/src/main/java/chess/MovesGenerator.java
+++ shared/src/main/java/chess/MovesGenerator.java
@@ -16,6 +16,14 @@ public class MovesGenerator {
         ownColor = board.getPiece(position).getTeamColor();
     }
 
+    public void checkAndAddPossibleMoves() {
+        for(ChessPosition newPosition : possibleMoves) {
+            if(newPosition.isInBounds() && (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != ownColor)) {
+                validMoves.add(new ChessMove(position, newPosition, null));
+            }
+        }
+    }
+
     Collection<ChessMove> getValidMoves() {
         return validMoves;
     }
