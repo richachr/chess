b36e76f7272ef607520d82e38fbaac61bbd08206
richachr <techzgamingdevs@gmail.com>
2025-05-26 18:21:23 -0600

    Implemented constructor, createUser, and clear. Added skeleton for getUser.

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,7 +1,49 @@
 package dataaccess;
 
-public class SQLUserDAO {
-    public SQLUserDAO() throws DataAccessException {
+import model.UserData;
+
+import java.sql.SQLException;
+
+public class SQLUserDAO implements UserDAO {
+    public SQLUserDAO() throws DataAccessException, SQLException {
         DatabaseManager.createDatabase();
+        String sql = """
+                     CREATE TABLE IF NOT EXISTS user (
+                     username VARCHAR(64) PRIMARY KEY NOT NULL,
+                     password VARCHAR(255) NOT NULL,
+                     email VARCHAR(255) NOT NULL
+                     );
+                     """;
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql);) {
+            statement.executeUpdate();
+        }
+    }
+
+    public void createUser(UserData data) throws DataAccessException {
+        String sql = """
+                     INSERT INTO  user (username, password, email)
+                     VALUES (?, ?, ?);
+                     """;
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql)) {
+            statement.setString(1, data.username());
+            statement.setString(2, data.password());
+            statement.setString(3, data.email());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getLocalizedMessage());
+        }
+    }
+
+    public UserData getUser(String username) {
+
+    }
+
+    public void clear() throws DataAccessException {
+        String sql = "DELETE FROM user;";
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getLocalizedMessage());
+        }
     }
 }
