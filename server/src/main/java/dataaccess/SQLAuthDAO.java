0352fcca4ec389a7255bf44ad955c16a1b5a72f5
richachr <techzgamingdevs@gmail.com>
2025-05-26 18:31:46 -0600

    Implemented clear method.

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,7 +1,18 @@
 package dataaccess;
 
+import java.sql.SQLException;
+
 public class SQLAuthDAO {
     public SQLAuthDAO() throws DataAccessException {
         DatabaseManager.createDatabase();
+    }
+
+    public void clear() throws DataAccessException {
+        String sql = "DELETE FROM auth;";
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getLocalizedMessage());
+        }
     }
 }
--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,8 +1,19 @@
 package dataaccess;
 
+import java.sql.SQLException;
+
 public class SQLGameDAO {
 
     public SQLGameDAO() throws DataAccessException {
         DatabaseManager.createDatabase();
+    }
+
+    public void clear() throws DataAccessException {
+        String sql = "DELETE FROM game;";
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getLocalizedMessage());
+        }
     }
 }
