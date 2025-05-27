6da35788e73958c3302398353d3d93400e769613
richachr <techzgamingdevs@gmail.com>
2025-05-27 14:11:46 -0600

    Implemented constructor, createAuth, getAuth, and deleteAuth methods.

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,10 +1,67 @@
 package dataaccess;
 
+import model.AuthData;
+
 import java.sql.SQLException;
 
-public class SQLAuthDAO {
+public class SQLAuthDAO implements AuthDAO {
     public SQLAuthDAO() throws DataAccessException {
         DatabaseManager.createDatabase();
+        String sql = """
+                     CREATE TABLE IF NOT EXISTS auth (
+                     username VARCHAR(64) NOT NULL,
+                     authToken CHAR(36) NOT NULL,
+                     PRIMARY KEY (username, auth_token)
+                     );
+                     """;
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getLocalizedMessage());
+        }
+    }
+
+    public void createAuth(AuthData data) throws DataAccessException {
+        String sql = """
+                     INSERT INTO  auth (username, auth_token)
+                     VALUES (?, ?);
+                     """;
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql)) {
+            statement.setString(1, data.username());
+            statement.setString(2, data.authToken());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getLocalizedMessage());
+        }
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        String sql = """
+                     SELECT * FROM auth
+                     WHERE auth_token = ?;
+                     """;
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql)) {
+            statement.setString(1, authToken);
+            var rs = statement.executeQuery();
+            rs.next();
+            return new AuthData(rs.getString("username"), rs.getString("authToken"));
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getLocalizedMessage());
+        }
+    }
+
+    public void deleteAuth(AuthData data) throws DataAccessException {
+        String sql = """
+                     DELETE FROM auth
+                     WHERE username = ? AND auth_token = ?;
+                     """;
+        try(var conn = DatabaseManager.getConnection(); var statement = conn.prepareStatement(sql)) {
+            statement.setString(1, data.username());
+            statement.setString(2, data.authToken());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getLocalizedMessage());
+        }
     }
 
     public void clear() throws DataAccessException {
