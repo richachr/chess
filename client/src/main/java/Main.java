f39bb1a397c158a6105162694628b0db7c7edc21
richachr <techzgamingdevs@gmail.com>
2025-05-30 14:07:34 -0600

    Added url and input loop implementation

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,7 +1,14 @@
 import chess.*;
+import ui.InputLoop;
 
 public class Main {
     public static void main(String[] args) {
+        String url = "http://localhost:8080";
+        if(args.length > 0) {
+            url = args[0];
+        }
+
+        new InputLoop().run();
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
     }
