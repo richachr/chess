2a5a181d420c1de882285dfe3c370b4263a1b6a7
richachr <techzgamingdevs@gmail.com>
2025-06-03 15:34:25 -0600

    Added username to client switch request.

--- client/src/main/java/ui/ClientSwitchRequest.java
+++ client/src/main/java/ui/ClientSwitchRequest.java
@@ -2,5 +2,5 @@ package ui;
 
 import chess.ChessGame;
 
-public record ClientSwitchRequest(String authToken, Integer gameId, ChessGame.TeamColor playerColor) {
+public record ClientSwitchRequest(String authToken, String username, Integer gameId, ChessGame.TeamColor playerColor) {
 }
