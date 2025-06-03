bcc765eeec513cd90a0d9e6bcc78a213c0733cd0
richachr <techzgamingdevs@gmail.com>
2025-06-03 10:40:18 -0600

    Implemented methods to write request body, read response body, and handle http errors. Added exceptions to method signatures.

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,12 +1,13 @@
 package server;
 
+import com.google.gson.Gson;
 import request.*;
 import result.CreateGameResult;
 import result.ListGamesResult;
 import result.LoginResult;
 import result.RegisterResult;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 
 public class ServerFacade {
@@ -16,54 +17,89 @@ public class ServerFacade {
         this.url = url;
     }
 
-    public RegisterResult register(RegisterRequest req) {
+    public RegisterResult register(RegisterRequest req) throws ResponseException {
         return this.sendRequest("/user", "POST", req, RegisterResult.class);
     }
 
-    public LoginResult login(LoginRequest req) {
+    public LoginResult login(LoginRequest req) throws ResponseException {
         return this.sendRequest("/session", "POST", req, LoginResult.class);
     }
 
-    public void logout(LogoutRequest req) {
+    public void logout(LogoutRequest req) throws ResponseException {
         this.sendRequest("/session", "DELETE", req, null);
     }
 
-    public ListGamesResult listGames(ListGamesRequest req) {
+    public ListGamesResult listGames(ListGamesRequest req) throws ResponseException {
         return this.sendRequest("/game", "GET", req, ListGamesResult.class);
     }
 
-    public CreateGameResult createGame(CreateGameRequest req) {
+    public CreateGameResult createGame(CreateGameRequest req) throws ResponseException {
         return this.sendRequest("/game", "POST", req, CreateGameResult.class);
     }
 
-    public void joinGame(JoinGameRequest req) {
+    public void joinGame(JoinGameRequest req) throws ResponseException {
         this.sendRequest("/game", "PUT", req, null);
     }
 
-    public <T> T sendRequest(String path, String method, Object request, Class<T> responseClass) {
+    public <T> T sendRequest(String path, String method, Object request, Class<T> responseClass) throws ResponseException {
         try {
             HttpURLConnection connection = getConnection(path);
             connection.setRequestMethod(method);
             connection.setDoOutput(true);
-            setBody(connection, request);
+            setReqBody(connection, request);
             connection.connect();
             if(connection.getResponseCode() != 200) {
                 handleErrors(connection);
             }
-            return getBody(connection, responseClass);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            return getResBody(connection, responseClass);
+        } catch (ResponseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ResponseException(500, e.getLocalizedMessage());
         }
     }
 
-    public HttpURLConnection getConnection(String path) {
+    public HttpURLConnection getConnection(String path) throws IOException {
         try {
             var requestUrl = new URI(this.url + path).toURL();
             return (HttpURLConnection) requestUrl.openConnection();
-        }  catch (URISyntaxException | IOException e) {
+        }  catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public void setReqBody(HttpURLConnection connection, Object request) throws IOException {
+        if(request != null) {
+            connection.addRequestProperty("Content-Type", "application/json");
+            String requestText = new Gson().toJson(request);
+            try(OutputStream out = connection.getOutputStream(); OutputStreamWriter writer = new OutputStreamWriter(out)) {
+                writer.write(requestText);
+            }
+        }
+    }
+
+    public <T> T getResBody(HttpURLConnection connection, Class<T> responseClass) throws IOException {
+        T response = null;
+        if(responseClass != null && connection.getContentLength() != 0) {
+            try(InputStream resBody = connection.getInputStream(); InputStreamReader reader = new InputStreamReader(resBody)) {
+                response = new Gson().fromJson(reader, responseClass);
+            }
+        }
+        return response;
+    }
+
+    public void handleErrors(HttpURLConnection connection) throws IOException, ResponseException {
+        int statusCode = connection.getResponseCode();
+        try(InputStream in = connection.getErrorStream();
+            InputStreamReader inputReader = new InputStreamReader(in);
+            BufferedReader reader = new BufferedReader(inputReader)) {
+            String line;
+            StringBuilder errorMsg = new StringBuilder();
+            while((line = reader.readLine()) != null) {
+                errorMsg.append(line);
+            }
+            throw new ResponseException(statusCode, errorMsg.toString());
+        }
+    }
 
 }
