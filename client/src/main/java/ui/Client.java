86b5d5cd431f25e875f55ed82d863bd914f0bb32
richachr <techzgamingdevs@gmail.com>
2025-06-03 13:34:07 -0600

    Added processInput and printError methods

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -2,4 +2,8 @@ package ui;
 
 public interface Client {
     public void printHelp();
+    public void processInput(String input);
+    public default void printError(String error) {
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_RED + "[ERROR]: " + error + EscapeSequences.RESET_TEXT_COLOR);
+    }
 }
