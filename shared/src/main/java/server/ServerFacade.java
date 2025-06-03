062e43b795dacab23d53ded0154faebfe330d50a
richachr <techzgamingdevs@gmail.com>
2025-06-02 20:29:58 -0600

    Added methods to receive data and begin a request

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,4 +1,41 @@
 package server;
 
+import request.*;
+import result.CreateGameResult;
+import result.ListGamesResult;
+import result.LoginResult;
+import result.RegisterResult;
+
 public class ServerFacade {
+    public String url;
+
+    public ServerFacade(String url) {
+        this.url = url;
+    }
+
+    public RegisterResult register(RegisterRequest req) {
+        this.sendRequest("/user", "POST", req, RegisterResult.class);
+    }
+
+    public LoginResult login(LoginRequest req) {
+        this.sendRequest("/user", "POST", req, RegisterResult.class);
+    }
+
+    public void logout(LogoutRequest req) {
+        this.sendRequest("/user", "POST", req, RegisterResult.class);
+    }
+
+    public ListGamesResult listGames(ListGamesRequest req) {
+        this.sendRequest("/user", "POST", req, RegisterResult.class);
+    }
+
+    public CreateGameResult createGame(CreateGameRequest req) {
+        this.sendRequest("/user", "POST", req, RegisterResult.class);
+    }
+
+    public void joinGame(JoinGameRequest req) {
+        this.sendRequest("/user", "POST", req, RegisterResult.class);
+    }
+
+
 }
