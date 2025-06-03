5aa2b26fd079065ae2e49dc5ebaf75677ac60891
richachr <techzgamingdevs@gmail.com>
2025-06-02 20:52:29 -0600

    Implemented sendRequest and getConnection methods

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -6,6 +6,9 @@ import result.ListGamesResult;
 import result.LoginResult;
 import result.RegisterResult;
 
+import java.io.IOException;
+import java.net.*;
+
 public class ServerFacade {
     public String url;
 
@@ -14,27 +17,52 @@ public class ServerFacade {
     }
 
     public RegisterResult register(RegisterRequest req) {
-        this.sendRequest("/user", "POST", req, RegisterResult.class);
+        return this.sendRequest("/user", "POST", req, RegisterResult.class);
     }
 
     public LoginResult login(LoginRequest req) {
-        this.sendRequest("/user", "POST", req, RegisterResult.class);
+        return this.sendRequest("/session", "POST", req, LoginResult.class);
     }
 
     public void logout(LogoutRequest req) {
-        this.sendRequest("/user", "POST", req, RegisterResult.class);
+        this.sendRequest("/session", "DELETE", req, null);
     }
 
     public ListGamesResult listGames(ListGamesRequest req) {
-        this.sendRequest("/user", "POST", req, RegisterResult.class);
+        return this.sendRequest("/game", "GET", req, ListGamesResult.class);
     }
 
     public CreateGameResult createGame(CreateGameRequest req) {
-        this.sendRequest("/user", "POST", req, RegisterResult.class);
+        return this.sendRequest("/game", "POST", req, CreateGameResult.class);
     }
 
     public void joinGame(JoinGameRequest req) {
-        this.sendRequest("/user", "POST", req, RegisterResult.class);
+        this.sendRequest("/game", "PUT", req, null);
+    }
+
+    public <T> T sendRequest(String path, String method, Object request, Class<T> responseClass) {
+        try {
+            HttpURLConnection connection = getConnection(path);
+            connection.setRequestMethod(method);
+            connection.setDoOutput(true);
+            setBody(connection, request);
+            connection.connect();
+            if(connection.getResponseCode() != 200) {
+                handleErrors(connection);
+            }
+            return getBody(connection, responseClass);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public HttpURLConnection getConnection(String path) {
+        try {
+            var requestUrl = new URI(this.url + path).toURL();
+            return (HttpURLConnection) requestUrl.openConnection();
+        }  catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
