f53dce2b687fcea22647927418b6a41bb9a03e87
richachr <techzgamingdevs@gmail.com>
2025-06-03 13:36:28 -0600

    Added processInput method, added login and register skeletons.

--- client/src/main/java/ui/LoggedOutClient.java
+++ client/src/main/java/ui/LoggedOutClient.java
@@ -1,12 +1,44 @@
 package ui;
 
+import server.ServerFacade;
+
+import java.util.Scanner;
+
 public class LoggedOutClient implements Client {
+    @Override
+    public void processInput(String input) {
+        try(Scanner inputScanner = new Scanner(input)) {
+            switch(inputScanner.next().toLowerCase().strip()) {
+                case "help" -> printHelp();
+                case "quit" -> {}
+                case "login" -> login(input);
+                case "register" -> register(input);
+                default -> printError("Unexpected command; type \"help\" to list valid commands.");
+            }
+        }
+
+    }
 
     @Override
     public void printHelp() {
         System.out.println(EscapeSequences.EMPTY + "help -> " + EscapeSequences.SET_TEXT_ITALIC + "display help text.");
         System.out.println(EscapeSequences.EMPTY + "quit -> " + EscapeSequences.SET_TEXT_ITALIC + "exit the program.");
-        System.out.println(EscapeSequences.EMPTY + "login [username] [password] -> " + EscapeSequences.SET_TEXT_ITALIC + "display help text.");
-        System.out.println(EscapeSequences.EMPTY + "register [username] [password] [email] -> " + EscapeSequences.SET_TEXT_ITALIC + "display help text.");
+        System.out.println(EscapeSequences.EMPTY + "login [username] [password] -> " + EscapeSequences.SET_TEXT_ITALIC + "log in as an existing user.");
+        System.out.println(EscapeSequences.EMPTY + "register [username] [password] [email] -> " + EscapeSequences.SET_TEXT_ITALIC + "register a new user.");
+        System.out.println(EscapeSequences.RESET_TEXT_ITALIC);
+    }
+
+    public void login(String input) {
+        try(Scanner inputScanner = new Scanner(input)) {
+            inputScanner.next();
+            try {
+                new ServerFacade()
+            }
+        }
+
+    }
+
+    public void register(String input) {
+
     }
 }
