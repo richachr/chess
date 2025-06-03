abe1fe1dd983f2fa97fb6c1c1cd0e293f8477fd6
richachr <techzgamingdevs@gmail.com>
2025-06-03 14:34:36 -0600

    Removed redundant return values

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -3,9 +3,9 @@ package ui;
 import server.ServerFacade;
 
 public interface Client {
-    public void printHelp();
-    public ClientSwitchRequest processInput(String input, ServerFacade facade);
-    public default void printError(String error) {
+    void printHelp();
+    ClientSwitchRequest processInput(String input, ServerFacade facade);
+    default void printError(String error) {
         System.out.println(EscapeSequences.SET_TEXT_COLOR_RED + "[ERROR]: " + error + EscapeSequences.RESET_TEXT_COLOR);
     }
 }
