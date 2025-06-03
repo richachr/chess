95d0590e42ac6f0bd2093aeed444a15ebec2a603
richachr <techzgamingdevs@gmail.com>
2025-06-03 10:39:37 -0600

    Initial commit

---
+++ shared/src/main/java/server/ResponseException.java
@@ -0,0 +1,16 @@
+package server;
+
+public class ResponseException extends Exception {
+    public Integer statusCode;
+
+    public ResponseException(String message) {
+        super(message);
+        this.statusCode = null;
+    }
+
+    public ResponseException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+}
