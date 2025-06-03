dac6d807accb9306f20a42872a6de67edc2e3992
richachr <techzgamingdevs@gmail.com>
2025-06-03 12:53:12 -0600

    Added printHelp method and implemented Client interface.

--- client/src/main/java/ui/LoggedInClient.java
+++ client/src/main/java/ui/LoggedInClient.java
@@ -1,4 +1,13 @@
 package ui;
 
-public class LoggedInClient {
+public class LoggedInClient implements Client {
+    @Override
+    public void printHelp() {
+        System.out.println(EscapeSequences.EMPTY + "help -> " + EscapeSequences.SET_TEXT_ITALIC + "display help text.");
+        System.out.println(EscapeSequences.EMPTY + "logout -> " + EscapeSequences.SET_TEXT_ITALIC + "log out of current session.");
+        System.out.println(EscapeSequences.EMPTY + "create [gameName] -> " + EscapeSequences.SET_TEXT_ITALIC + "create a game with the specified name.");
+        System.out.println(EscapeSequences.EMPTY + "list -> " + EscapeSequences.SET_TEXT_ITALIC + "list all current games.");
+        System.out.println(EscapeSequences.EMPTY + "join [gameNumber] [color] -> " + EscapeSequences.SET_TEXT_ITALIC + "join a game as the specified color.");
+        System.out.println(EscapeSequences.EMPTY + "observe [gameNumber] -> " + EscapeSequences.SET_TEXT_ITALIC + "join a game as an observer.");
+    }
 }
--- client/src/main/java/ui/LoggedOutClient.java
+++ client/src/main/java/ui/LoggedOutClient.java
@@ -1,4 +1,12 @@
 package ui;
 
-public class LoggedOutClient {
+public class LoggedOutClient implements Client {
+
+    @Override
+    public void printHelp() {
+        System.out.println(EscapeSequences.EMPTY + "help -> " + EscapeSequences.SET_TEXT_ITALIC + "display help text.");
+        System.out.println(EscapeSequences.EMPTY + "quit -> " + EscapeSequences.SET_TEXT_ITALIC + "exit the program.");
+        System.out.println(EscapeSequences.EMPTY + "login [username] [password] -> " + EscapeSequences.SET_TEXT_ITALIC + "display help text.");
+        System.out.println(EscapeSequences.EMPTY + "register [username] [password] [email] -> " + EscapeSequences.SET_TEXT_ITALIC + "display help text.");
+    }
 }
