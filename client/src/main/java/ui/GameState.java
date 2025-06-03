8b28d2b3793a9bd2cef20970f4b766774a8f0760
richachr <techzgamingdevs@gmail.com>
2025-06-03 12:52:39 -0600

    Added method to get a client based on game state.

--- client/src/main/java/ui/GameState.java
+++ client/src/main/java/ui/GameState.java
@@ -3,5 +3,15 @@ package ui;
 public enum GameState {
     LOGGED_IN,
     LOGGED_OUT,
-    IN_GAME
+    IN_GAME;
+
+    public Client getClient() {
+        Client clientObject = null;
+        switch(this) {
+            case IN_GAME -> clientObject = new InGameClient();
+            case LOGGED_IN -> clientObject = new LoggedInClient();
+            case LOGGED_OUT -> clientObject = new LoggedOutClient();
+        }
+        return clientObject;
+    }
 }
