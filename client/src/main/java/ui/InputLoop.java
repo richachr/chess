ced60aa4615b83cf3c03938383d85c35048859da
richachr <techzgamingdevs@gmail.com>
2025-06-03 12:53:57 -0600

    Added game state, prefix for console, and basic run method.

--- client/src/main/java/ui/InputLoop.java
+++ client/src/main/java/ui/InputLoop.java
@@ -1,4 +1,21 @@
 package ui;
 
+import java.util.Scanner;
+
+import static ui.GameState.*;
+
 public class InputLoop {
+    GameState state = LOGGED_OUT;
+    String prefix = state.toString();
+
+    public void run() {
+        String userInput;
+        Scanner inputScanner = new Scanner(System.in);
+        do {
+            System.out.printf(" %s > " + EscapeSequences.SET_TEXT_BLINKING, prefix);
+            userInput = inputScanner.nextLine();
+            System.out.print(EscapeSequences.RESET_TEXT_BLINKING);
+        } while (!userInput.equalsIgnoreCase("quit"));
+    }
+
 }
