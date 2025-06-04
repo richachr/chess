4eed98804069581340f072d37bde37152f96f77a
richachr <techzgamingdevs@gmail.com>
2025-06-04 17:37:32 -0600

    Implemented tests for all public methods of ServerFacade (except sendRequest)

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,6 +1,8 @@
 package client;
 
+import model.GameData;
 import org.junit.jupiter.api.*;
+import request.*;
 import server.ResponseException;
 import server.Server;
 import server.ServerFacade;
@@ -14,9 +16,9 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8008);
         System.out.println("Started test HTTP server on " + port);
-        facade = new ServerFacade("http://localhost:8080");
+        facade = new ServerFacade("http://localhost:8008");
     }
 
     @BeforeEach
@@ -31,63 +33,89 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void successfulRegisterTest() {
-        Assertions.assertTrue(true);
+    public void successfulRegisterTest() throws ResponseException {
+        var res = facade.register(new RegisterRequest("user", "pass", "email"));
+        Assertions.assertNotNull(res.authToken());
+        Assertions.assertEquals("user", res.username());
     }
 
     @Test
     public void failedRegisterTest() {
-        Assertions.assertTrue(true);
+        Assertions.assertThrows(ResponseException.class, () ->
+                facade.register(new RegisterRequest(null, "good pass", "fake email")));
     }
 
     @Test
-    public void successfulLoginTest() {
-        Assertions.assertTrue(true);
+    public void successfulLoginTest() throws ResponseException {
+        facade.register(new RegisterRequest("user", "pass", "email"));
+        var res = facade.login(new LoginRequest("user", "pass"));
+        Assertions.assertNotNull(res.authToken());
+        Assertions.assertEquals("user", res.username());
     }
 
     @Test
     public void failedLoginTest() {
-        Assertions.assertTrue(true);
+        Assertions.assertThrows(ResponseException.class, () ->
+                facade.login(new LoginRequest("random blone", "pass")));
     }
 
     @Test
-    public void successfulLogoutTest() {
-        Assertions.assertTrue(true);
+    public void successfulLogoutTest() throws ResponseException {
+        var registerRes = facade.register(new RegisterRequest("user", "pass", "email"));
+        facade.logout(new LogoutRequest(registerRes.authToken()));
+        Assertions.assertThrows(ResponseException.class, () ->
+                facade.logout(new LogoutRequest(registerRes.authToken())));
     }
 
     @Test
     public void failedLogoutTest() {
-        Assertions.assertTrue(true);
+        Assertions.assertThrows(ResponseException.class, () ->
+                facade.logout(new LogoutRequest("fake token")));
     }
 
     @Test
-    public void successfulListGamesTest() {
-        Assertions.assertTrue(true);
+    public void successfulListGamesTest() throws ResponseException {
+        var registerRes = facade.register(new RegisterRequest("user", "pass", "email"));
+        var res = facade.listGames(new ListGamesRequest(registerRes.authToken()));
+        Assertions.assertInstanceOf(GameData[].class, res.games());
+        Assertions.assertEquals(0, res.games().length);
     }
 
     @Test
     public void failedListGamesTest() {
-        Assertions.assertTrue(true);
+        Assertions.assertThrows(ResponseException.class, () ->
+                facade.listGames(new ListGamesRequest("fake token")));
     }
 
     @Test
-    public void successfulCreateGameTest() {
-        Assertions.assertTrue(true);
+    public void successfulCreateGameTest() throws ResponseException {
+        var registerRes = facade.register(new RegisterRequest("user", "pass", "email"));
+        var res = facade.createGame(new CreateGameRequest("game", registerRes.authToken()));
+        Assertions.assertNotNull(res.gameID());
     }
 
     @Test
-    public void failedCreateGameTest() {
-        Assertions.assertTrue(true);
+    public void failedCreateGameTest() throws ResponseException {
+        var registerRes = facade.register(new RegisterRequest("user", "pass", "email"));
+        Assertions.assertThrows(ResponseException.class, () ->
+                facade.createGame(new CreateGameRequest(null, registerRes.authToken())));
     }
 
     @Test
-    public void successfulJoinGameTest() {
-        Assertions.assertTrue(true);
+    public void successfulJoinGameTest() throws ResponseException {
+        var registerRes = facade.register(new RegisterRequest("user", "pass", "email"));
+        var createRes = facade.createGame(new CreateGameRequest("game", registerRes.authToken()));
+        var beginningGamesList = facade.listGames(new ListGamesRequest(registerRes.authToken()));
+        facade.joinGame(new JoinGameRequest(createRes.gameID(), "WHITE", registerRes.authToken()));
+        Assertions.assertNotEquals(beginningGamesList, facade.listGames(new ListGamesRequest(registerRes.authToken())));
     }
 
     @Test
-    public void failedJoinGameTest() {
-        Assertions.assertTrue(true);
+    public void failedJoinGameTest() throws ResponseException {
+        var registerRes = facade.register(new RegisterRequest("user", "pass", "email"));
+        var createRes = facade.createGame(new CreateGameRequest("game", registerRes.authToken()));
+        Assertions.assertThrows(ResponseException.class, () ->
+                facade.joinGame(new JoinGameRequest(createRes.gameID(), "blue", registerRes.authToken())));
     }
 
 }
