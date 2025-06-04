5e35488085d379d8fb31f01ea5f34a4f81f25b94
richachr <techzgamingdevs@gmail.com>
2025-06-04 17:15:35 -0600

    Added clear method to be run before each test, added test method skeletons

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,27 @@
 package client;
 
 import org.junit.jupiter.api.*;
+import server.ResponseException;
 import server.Server;
+import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:8080");
+    }
+
+    @BeforeEach
+    public void clear() throws ResponseException {
+        facade.sendRequest("/db", "DELETE", null, null);
     }
 
     @AfterAll
@@ -22,7 +31,62 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
+    public void successfulRegisterTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void failedRegisterTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void successfulLoginTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void failedLoginTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void successfulLogoutTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void failedLogoutTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void successfulListGamesTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void failedListGamesTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void successfulCreateGameTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void failedCreateGameTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void successfulJoinGameTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void failedJoinGameTest() {
         Assertions.assertTrue(true);
     }
 
