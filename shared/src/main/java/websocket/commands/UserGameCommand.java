53206ac1738eeb2eea54d56a4d0805aeb1bd5afa
richachr <techzgamingdevs@gmail.com>
2025-06-12 17:48:14 -0600

    Added additional necessary instance variables, constructors, and getters

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,5 +1,9 @@
 package websocket.commands;
 
+import chess.ChessGame;
+import chess.ChessMove;
+import websocket.ClientData;
+
 import java.util.Objects;
 
 /**
@@ -16,10 +20,50 @@ public class UserGameCommand {
 
     private final Integer gameID;
 
+    private final ChessGame.TeamColor color;
+
+    private final ChessMove move;
+
     public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
+        this.color = null;
+        this.move = null;
+    }
+
+    // May need type adapter to convert parameters to TeamColor and ChessMove objects.
+
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, ChessGame.TeamColor color) {
+        this.commandType = commandType;
+        this.authToken = authToken;
+        this.gameID = gameID;
+        this.color = color;
+        this.move = null;
+    }
+
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, ChessGame.TeamColor color, ChessMove move) {
+        this.commandType = commandType;
+        this.authToken = authToken;
+        this.gameID = gameID;
+        this.color = color;
+        this.move = move;
+    }
+
+    public UserGameCommand(CommandType commandType, ClientData data) {
+        this.commandType = commandType;
+        this.authToken = data.authToken();
+        this.color = data.playerColor();
+        this.gameID = data.gameId();
+        this.move = null;
+    }
+
+    public UserGameCommand(CommandType commandType, ClientData data, ChessMove move) {
+        this.commandType = commandType;
+        this.authToken = data.authToken();
+        this.color = data.playerColor();
+        this.gameID = data.gameId();
+        this.move = move;
     }
 
     public enum CommandType {
@@ -39,6 +83,14 @@ public class UserGameCommand {
 
     public Integer getGameID() {
         return gameID;
+    }
+
+    public ChessGame.TeamColor getColor() {
+        return color;
+    }
+
+    public ChessMove getMove() {
+        return move;
     }
 
     @Override
