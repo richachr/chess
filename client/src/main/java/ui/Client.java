8ac8979c2a9e2d001770588d20cbce02d7f24346
richachr <techzgamingdevs@gmail.com>
2025-06-11 18:08:46 -0600

    Added spacing

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -4,7 +4,9 @@ import facade.ServerFacade;
 
 public interface Client {
     void printHelp();
+
     ClientSwitchRequest processInput(String input, ServerFacade facade);
+
     default void printError(String error) {
         System.out.println(EscapeSequences.SET_TEXT_COLOR_RED + "[ERROR]: " + error + EscapeSequences.RESET_TEXT_COLOR);
     }
