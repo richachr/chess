511660b280a9ff3b2d822e2747c0d3432c85fe34
richachr <techzgamingdevs@gmail.com>
2025-06-12 18:20:09 -0600

    Added remove and send methods

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,7 +1,10 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -15,6 +18,43 @@ public class ConnectionManager {
             gameConnections = new ArrayList<>();
         }
         gameConnections.add(conn);
+        connections.put(gameId, gameConnections);
+    }
+
+    public void remove(String username, Integer gameId) {
+        var gameConnections = connections.get(gameId);
+        if(gameConnections == null) {
+            return;
+        }
+        gameConnections.removeIf((conn) -> conn.username().equalsIgnoreCase(username));
+        connections.put(gameId, gameConnections);
+    }
+
+    public void sendAndExclude(String userToExclude, Integer gameId, ServerMessage message) throws IOException {
+        var gameConnections = connections.get(gameId);
+        for(Connection conn : gameConnections) {
+            if(conn.session().isOpen()) {
+                if(!conn.username().equalsIgnoreCase(userToExclude)) {
+                    conn.sendMessage(new Gson().toJson(message));
+                }
+            } else {
+                gameConnections.remove(conn);
+            }
+        }
+        connections.put(gameId, gameConnections);
+    }
+
+    public void sendMessageToUser(String user, Integer gameId, ServerMessage message) throws IOException {
+        var gameConnections = connections.get(gameId);
+        for(Connection conn : gameConnections) {
+            if(conn.session().isOpen()) {
+                if(conn.username().equalsIgnoreCase(user)) {
+                    conn.sendMessage(new Gson().toJson(message));
+                }
+            } else {
+                gameConnections.remove(conn);
+            }
+        }
         connections.put(gameId, gameConnections);
     }
 }
