8ff22a52b430f09865026418b786d4a0f9efabc4
richachr <techzgamingdevs@gmail.com>
2025-06-12 18:01:48 -0600

    Added add method

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -8,5 +8,13 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionManager {
     public final ConcurrentHashMap<Integer, ArrayList<Connection>> connections = new ConcurrentHashMap<>();
 
-
+    public void add(Session session, String username, Integer gameId) {
+        Connection conn = new Connection(session, username);
+        var gameConnections = connections.get(gameId);
+        if(gameConnections == null) {
+            gameConnections = new ArrayList<>();
+        }
+        gameConnections.add(conn);
+        connections.put(gameId, gameConnections);
+    }
 }
